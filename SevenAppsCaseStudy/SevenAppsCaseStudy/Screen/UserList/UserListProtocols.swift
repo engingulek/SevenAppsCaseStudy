3be40f34fb7055dd5c0ca1ccd9b597c0698fbafb
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-03-05 14:26:52 +0300

    Created First UserListProtocols

---
+++ SevenAppsCaseStudy/SevenAppsCaseStudy/Screen/UserList/UserListProtocols.swift
@@ -0,0 +1,24 @@
+//
+//  UserListProtocols.swift
+//  SevenAppsCaseStudy
+//
+//  Created by Engin Gülek on 5.03.2025.
+//
+
+import Foundation
+
+// MARK: - UserListViewModelInputProtocol
+/// The input protocol for the UserListViewModel.
+protocol UserListViewModelInputProtocol   {
+    var delegate : UserListViewModelOutputProtocol? {set get}
+    func viewDidLoad()
+    func numberOfItems() -> Int
+    func cellForItem(at indexPath:IndexPath) -> User
+}
+
+// MARK: - UserListViewModelOutputProtocol
+/// The output protocol for the UserListViewModel.
+protocol UserListViewModelOutputProtocol : AnyObject {
+    func prepareTableView()
+    func reloadTableView()
+}
