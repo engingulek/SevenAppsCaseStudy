4ebc40024a86a819a18b8aa938b51afb08d6f2d6
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-03-05 14:05:12 +0300

    Created Project

---
+++ SevenAppsCaseStudy/SevenAppsCaseStudy/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  SevenAppsCaseStudy
+//
+//  Created by Engin Gülek on 5.03.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController =  ViewController()
+        window?.makeKeyAndVisible()
+        
+    }
+}
+
---
+++ SevenAppsCaseStudy/SevenAppsCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCaseStudy
+//
+//  Created by Engin Gülek on 5.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
